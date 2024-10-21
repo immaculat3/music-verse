c859c2b611673bfa685d3a59905718a6f6a74f2a
Felixdiamond <diamondfelix006@gmail.com>
2024-10-21 15:56:38 +0100

    Refactor LeftSideBar component to add new icons and adjust padding

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,10 +1,14 @@
 import { RiVoiceprintLine } from "react-icons/ri";
 import { RiPlayListFill } from "react-icons/ri";
 import { RxBarChart } from "react-icons/rx";
+import { FaRegHeart } from "react-icons/fa";
+import { GoClock } from "react-icons/go";
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaPodcast } from "react-icons/fa";
 
 export function LeftSideBar() {
   return (
-    <div className="flex flex-col rounded-lg w-1/5 h-screen pl-5 gap-y-10">
+    <div className="flex flex-col rounded-lg w-1/5 h-screen pl-7 gap-y-10">
       <div>
         <h1 className="pt-5">Music Verse</h1>
       </div>
@@ -20,6 +24,25 @@ export function LeftSideBar() {
         <button className="flex items-center">
           <RxBarChart className="mr-2" />
           <span>Statistics</span>
+        </button>
+      </div>
+      <div className="flex flex-col gap-y-5">
+        <span className="text-xs uppercase">Your Music</span>
+        <button className="flex items-center">
+          <FaRegHeart className="mr-2" />
+          <span>Favorites</span>
+        </button>
+        <button className="flex items-center">
+          <GoClock className="mr-2" />
+          <span>Listen Later</span>
+        </button>
+        <button className="flex items-center">
+          <FaCalendarAlt className="mr-2" />
+          <span>History</span>
+        </button>
+        <button className="flex items-center">
+          <FaPodcast className="mr-2" />
+          <span>Podcasts</span>
         </button>
       </div>
     </div>
